949cfbf36bdd1f0b81bdc398db6e65505791e2a2
joenzkc <jokichaa@gmail.com>
2023-08-11 23:58:15 +0800

    add home page - shorten url from anonymous user - display shortened url, copy to clipboard - sign up button redirect to sign up page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,112 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import Container from "postcss/lib/container";
 
-const inter = Inter({ subsets: ['latin'] })
+import Container from "@/components/Container";
+import { createAnonUrl } from "@/components/api/apiHelper";
+import { ValidUrlDto } from "@/components/util/ValidUrlDto";
+import { plainToClass } from "class-transformer";
+import { ValidationError, validateOrReject } from "class-validator";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [error, setError] = useState("");
+  const [invalidUrl, setInvalidUrl] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  };
+
+  const onClickShorten = async () => {
+    try {
+      const dto: ValidUrlDto = plainToClass(ValidUrlDto, { url });
+      await validateOrReject(dto);
+      const res = await createAnonUrl(url);
+      setShortUrl(res.hashUrl);
+    } catch (err: any) {
+      if (err.length > 0) {
+        if (err[0] instanceof ValidationError) {
+          console.log("validation error");
+          setInvalidUrl(true);
+        }
+      }
+      console.log(err);
+    }
+  };
+
+  const onClickCopy = () => {
+    // copy to clipboard the test in the span
+    const text = `${process.env.NEXT_PUBLIC_FRONTEND_URL}${shortUrl}`;
+    navigator.clipboard.writeText(text);
+  };
+
+  const onClickSignUp = () => {
+    router.push("/signup");
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <Container>
+      <div className="flex flex-col flex-1 p-2 w-full max-w-2xl">
+        <div className="bg-white rounded-md p-4 flex flex-col">
+          <div className=" min-h-[2rem]">
+            <p className="font-lato font-light">Try shortening a URL!</p>
+          </div>
+          <div className="mt-1 flex-1 flex">
+            <input
+              className={`border-2 ${
+                invalidUrl || error ? "border-red-500" : "border-gray-300"
+              } bg-white h-10 px-2 pr-16 rounded-lg text-md flex-1 focus:outline-none`}
+              type="search"
+              name="search"
+              placeholder="Enter your link here..."
+              onChange={handleChange}
             />
-          </a>
+          </div>
+          <div className="my-2 flex flex-col flex-1">
+            {invalidUrl ? <p className="text-red-500 mb-2">Invalid url</p> : ""}
+            <button
+              className="flex-1 bg-violet_purple rounded-xl min-h-[3rem]"
+              onClick={onClickShorten}
+            >
+              <p className=" text-lg text-gray-50">Shorten!</p>
+            </button>
+          </div>
+          {/* <div className="bg-red-100">Test</div> */}
+        </div>
+
+        {shortUrl !== "" ? (
+          <div className="bg-white rounded-md mt-2 p-4 flex flex-col min-h-[6rem]">
+            <p>Your shortened url is:</p>
+            <span className="bg-gray-300 flex-1 h-10 px-2 rounded-lg text-md flex justify-center items-center">
+              <p className="truncate flex-1">
+                {process.env.NEXT_PUBLIC_FRONTEND_URL}
+                {shortUrl}
+              </p>
+              <img
+                alt=""
+                src="/content-copy.png"
+                className="self-center w-6 h-6 hover:cursor-pointer"
+                onClick={onClickCopy}
+              />
+            </span>
+          </div>
+        ) : (
+          <></>
+        )}
+
+        <div className="bg-white rounded-md mt-2 p-4 flex flex-col">
+          <p>Want to make your own custom URL?</p>
+          <button
+            className="my-2 flex-1 bg-bright_yellow rounded-xl min-h-[3rem]"
+            onClick={onClickSignUp}
+          >
+            <p className=" text-lg text-gray-50">Sign up</p>
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </Container>
+  );
 }
