561a9136e7898270969f3ea08944f2787a46b02f
joenzkc <jokichaa@gmail.com>
2023-08-13 12:16:02 +0800

    add title for sign up page, encapsulate in form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container";
 import { signUp } from "@/components/api/apiHelper";
 import { LoginDto } from "@/components/util/LoginDto";
 import { validateOrReject } from "class-validator";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 
@@ -20,6 +21,10 @@ const signup = () => {
 
   const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
+  };
+
+  const onClickSignIn = () => {
+    router.push("/login");
   };
 
   const onClickSignUp = async () => {
@@ -49,76 +54,99 @@ const signup = () => {
     }
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <Container>
-      <div className="flex flex-col flex-1 p-2 w-full max-w-2xl">
-        <div className="grid container grid-cols-1 bg-white rounded-md p-4">
-          <h1 className="text-lg font-archivo text-center">Welcome</h1>
-          <div className="">
-            <div className="flex flex-col flex-1">
-              <p className="my-2 font-montserrat text-gray-800">Name</p>
-              <input
-                type="text"
-                className="bg-gray-50 border border-gray-400 rounded-lg px-2 h-10 focus:outline-none"
-                placeholder="Enter your name"
-                onChange={handleChangeName}
-              />
-            </div>
-            <div className="flex flex-col flex-1">
-              <p className="my-2 font-montserrat text-gray-800">Password</p>
-              <input
-                type="password"
-                className="bg-gray-50 border border-gray-400 rounded-lg px-2 h-10 focus:outline-none"
-                placeholder="Enter your name"
-                onChange={handleChangePassword}
-              />
-            </div>
-            {isError ? (
-              <div>
-                <p className="text-red-500 mt-1">
-                  Password must be at least 8 characters long and less than 20
-                  characters
-                </p>
+    <>
+      <Head>
+        <title>JURL: Sign up</title>
+      </Head>
+      <Container>
+        <form
+          onSubmit={handleSubmit}
+          className="flex-flex-col flex-1 p-2 w-full max-w-2xl"
+        >
+          {/* <div className="flex flex-col flex-1 p-2 w-full max-w-2xl"> */}
+          <div className="grid container grid-cols-1 bg-white rounded-xl p-4 shadow-lg">
+            <h1 className="text-lg font-archivo text-center">Welcome</h1>
+            <div className="">
+              <div className="flex flex-col flex-1">
+                <p className="my-2 font-montserrat text-gray-800">Name</p>
+                <input
+                  type="text"
+                  className="bg-gray-50 border border-gray-400 rounded-lg px-2 h-10 focus:outline-none"
+                  placeholder="Enter your name"
+                  onChange={handleChangeName}
+                />
               </div>
-            ) : (
-              ""
-            )}
-            {userTaken ? (
-              <div>
-                <p className="text-red-500 mt-1">
-                  That username has already been taken
-                </p>
+              <div className="flex flex-col flex-1">
+                <p className="my-2 font-montserrat text-gray-800">Password</p>
+                <input
+                  type="password"
+                  className="bg-gray-50 border border-gray-400 rounded-lg px-2 h-10 focus:outline-none"
+                  placeholder="Enter your name"
+                  onChange={handleChangePassword}
+                />
               </div>
-            ) : (
-              ""
-            )}
-          </div>
-          <div>
-            <button
-              className="bg-bright_pink rounded-xl mt-3 min-h-[3rem] w-full"
-              onClick={onClickSignUp}
-            >
-              {isLoading ? (
-                <div className="flex flex-1 items-center justify-center">
-                  <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-blue-500"></div>
+              {isError ? (
+                <div>
+                  <p className="text-red-500 mt-1">
+                    Password must be at least 8 characters long and less than 20
+                    characters
+                  </p>
                 </div>
               ) : (
-                <p className=" text-lg text-gray-50">Sign Up</p>
+                ""
               )}
-            </button>
-          </div>
-          {isSignedUp ? (
+              {userTaken ? (
+                <div>
+                  <p className="text-red-500 mt-1">
+                    That username has already been taken
+                  </p>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
             <div>
-              <p className="text-green-500 mt-1">
-                You have successfully signed up! Redirecting to login page...
+              <button
+                className="bg-bright_pink rounded-xl mt-3 min-h-[3rem] w-full"
+                type="submit"
+                onClick={onClickSignUp}
+              >
+                {isLoading ? (
+                  <div className="flex flex-1 items-center justify-center">
+                    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-blue-500"></div>
+                  </div>
+                ) : (
+                  <p className=" text-lg text-gray-50">Sign Up</p>
+                )}
+              </button>
+            </div>
+            <div className="mt-1">
+              <p
+                className="text-blue-500 cursor-pointer"
+                onClick={onClickSignIn}
+              >
+                Already a user? Sign in!
               </p>
             </div>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-    </Container>
+            {isSignedUp ? (
+              <div>
+                <p className="text-green-500 mt-1">
+                  You have successfully signed up! Redirecting to login page...
+                </p>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+          {/* </div> */}
+        </form>
+      </Container>
+    </>
   );
 };
 
